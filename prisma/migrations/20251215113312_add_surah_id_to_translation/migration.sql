bc18013cd781842daa0001005f5e19342ce11b82
akmmubashir <akmmubashir@gmail.com>
2026-01-13 19:13:09 +0530

    changes

--- prisma/migrations/20251215113312_add_surah_id_to_translation/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE "Translation" ADD COLUMN     "surahId" INTEGER;
-
--- CreateIndex
-CREATE INDEX "Translation_surahId_idx" ON "Translation"("surahId");
-
--- AddForeignKey
-ALTER TABLE "Translation" ADD CONSTRAINT "Translation_surahId_fkey" FOREIGN KEY ("surahId") REFERENCES "Surah"("id") ON DELETE SET NULL ON UPDATE CASCADE;
--- prisma/migrations/20251215113945_add_surah_id_to_tafsir/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE "Tafsir" ADD COLUMN     "surahId" INTEGER;
-
--- CreateIndex
-CREATE INDEX "Tafsir_surahId_idx" ON "Tafsir"("surahId");
-
--- AddForeignKey
-ALTER TABLE "Tafsir" ADD CONSTRAINT "Tafsir_surahId_fkey" FOREIGN KEY ("surahId") REFERENCES "Surah"("id") ON DELETE SET NULL ON UPDATE CASCADE;
